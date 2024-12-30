24386d04e3914a89f296737deee0d82db4a51950
Kari Jyrkkä <kajyrkka@oamk.fi>
2024-12-30 10:06:15 +0200

    muokattu esimerkkia

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
 using namespace std;
+void printLetters(string);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    string input;
+
+    cout << "Give text I will count letters in it!" << endl;
+    cin >> input;
+    printLetters(input);
+
     return 0;
+}
+
+void printLetters(string in)
+{
+    for(auto iterator = in.begin(); iterator!=in.end();iterator++)
+    {
+        cout<<"merkki = "<< *iterator << endl;
+    }
+    size_t lkm = in.length();
+    cout<<"merkkien lukumaara = " << lkm << endl;
 }
